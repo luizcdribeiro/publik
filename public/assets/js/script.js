ecdfc8f67ee7ad48cd7ce5f18e01dfce150023e6
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2020-07-30 10:24:33 -0300

    Componente formulario

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -11,7 +11,32 @@ $(document).ready(function(){
   $('.depoimento-slide-container').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
   $('.clientes-container').slick({
